91aa065231dec78a08cda7cfffa9a4026b18f34d
Auvic <victor.trani1@gmail.com>
2021-11-09 22:00:31 -0300

    Developing

--- app.js
+++ app.js
@@ -3,55 +3,93 @@ const searchBtn = document.getElementById("search-btn")
 const card = document.getElementById("card-container")
 
 // Event Listeners
-searchBtn.addEventListener("click", getUser)
+searchBtn.addEventListener("click", getCard)
 
 //Functions
-function getUser() {
+function getCard() {
     let userName = document.getElementById("user-name").value.trim()
-    fetch(`https://api.github.com/users/${userName}`)
-        .then(response => response.json())
+    const urlCard = fetch(`https://api.github.com/users/${userName}`)
+    urlCard.then(response => response.json())
         .then(data => {
             if (data.login) {
-
                 let html = `<div class="card">
                 <div class="card-top">
                 </div>
                 <div class="card-img">
     
-                    <img src="who.jpg" alt="User image" width="180px" height="180px">
+                    <img src="${data.avatar_url} alt="User image" width="180px" height="180px">
                 </div>
-                <h4 class="card-location"><i class="fas fa-map-marker-alt"></i> Somewhere</h4>
-                <h1 class="card-title">Some Body</h1>
+                <h4 class="card-location"><i class="fas fa-map-marker-alt"></i> ${data.location}</h4>
+                <a href="${data.blog}"class="card-title">${data.name}</a>
                 <div class="bio">
-                    <p> Description </p>
+                    <p>"${data.bio}"</p>
                 </div>
                 <div class="card-content">
-                    <div class="content-left">
                         <h3>Nick-Name:</h3>
-                        <p>Nick</p>
-                    </div>
-                    <div class="content-right">
-                        <h3>Latest Repositories:</h3>
-                        <ul>
-                            <li class="">#</li>
-                            <li class="">#</li>
-                            <li class="">#</li>
-                            <li class="">#</li>
-                        </ul>
-                    </div>
+                        <p>${data.login}</p>
+                        <h3>Repositories</h3>
                 </div>
                 <div class="content-lower">
                     <div class="followers">
-                        <p>X Followers <i class="fas fa-users"></i></p>
+                        <p>${data.followers} Followers <i class="fas fa-users"></i></p>
                     </div>
                     <div class="following">
-                        <p>Y Following <i class="fas fa-user"></i></p>
+                        <p>${data.following} Following <i class="fas fa-user"></i></p>
                     </div>
                 </div>
             </div>`
                 card.innerHTML = html
+            } else {
+                alert("Invalid User")
             }
-
         });
 
+
 }
+
+function getRepo() {
+    const urlRepo = fetch(`https://api.github.com/users/${userName}/repos`)
+    urlRepo.then(response => response.json())
+        .then(data => {
+
+                for (var i = 0; i < 5; i++) {
+                    function getCard() {
+                        let userName = document.getElementById("user-name").value.trim()
+                        const urlCard = fetch(`https://api.github.com/users/${userName}`)
+                        urlCard.then(response => response.json())
+                            .then(data => {
+                                if (data.login) {
+                                    let html = `<div class="card">
+                                <div class="card-top">
+                                </div>
+                                <div class="card-img">
+                    
+                                    <img src="${data.avatar_url} alt="User image" width="180px" height="180px">
+                                </div>
+                                <h4 class="card-location"><i class="fas fa-map-marker-alt"></i> ${data.location}</h4>
+                                <a href="${data.blog}"class="card-title">${data.name}</a>
+                                <div class="bio">
+                                    <p>"${data.bio}"</p>
+                                </div>
+                                <div class="card-content">
+                                        <h3>Nick-Name:</h3>
+                                        <p>${data.login}</p>
+                                        <h3>Repositories</h3>
+                                </div>
+                                <div class="content-lower">
+                                    <div class="followers">
+                                        <p>${data.followers} Followers <i class="fas fa-users"></i></p>
+                                    </div>
+                                    <div class="following">
+                                        <p>${data.following} Following <i class="fas fa-user"></i></p>
+                                    </div>
+                                </div>
+                            </div>`
+                                    card.innerHTML = html
+                                } else {
+                                    alert("Invalid User")
+                                }
+                            });
+                    }
+                });
+        }
