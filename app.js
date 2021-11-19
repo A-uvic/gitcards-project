5e11e26fb51763d3c0eac0ee91982abe7370ae3f
Auvic <victor.trani1@gmail.com>
2021-11-18 21:31:41 -0300

    Project part 1 finished

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ function getCard() {
     urlCard.then(response => response.json())
         .then(data => {
             if (data.login) {
+                let following = data.following
+                let followers = data.followers
+
                 let html = `<div class="card">
                 <div class="card-top">
                 </div>
                 <div class="card-img">
-    
                     <img src="${data.avatar_url} alt="User image" width="180px" height="180px">
                 </div>
                 <h4 class="card-location"><i class="fas fa-map-marker-alt"></i> ${data.location}</h4>
@@ -26,38 +28,42 @@ function getCard() {
                     <p>"${data.bio}"</p>
                 </div>
                 <div class="card-content">
-                        <h3>Latest Repositories</h3>
-                    <div className="repos">
-                    </div>
-                </div>
-                <div class="content-lower">
-                    <div class="followers">
-                        <p>${data.followers} Followers <i class="fas fa-users"></i></p>
-                    </div>
-                    <div>${data.login}</div>
-                    <div class="following">
-                        <p>${data.following} Following <i class="fas fa-user"></i></p>
-                    </div>
-                </div>
-            </div>`
+                        <h3>Latest Repositories</h3><div class="repos">`
 
-                getRepo(`${userName}`, html)
+                getRepo(`${userName}`, html, followers, following)
+
+
+                card.innerHTML = html
             } else {
                 alert("Invalid User")
             }
+
+
         });
 
 
 }
 
-function getRepo(userName, html) {
+function getRepo(userName, html, following, followers) {
     fetch(`https://api.github.com/users/${userName}/repos`)
         .then(response => response.json())
         .then(data => {
-
             for (var i = 1; i < 5; i++) {
-                html += `<li>${data[i].name}</li>`
+                html += `<a target="_blank" href="${data[i].html_url}"><p>${data[i].name}</p></a>`
             }
+
+            html += `</div>
+            <div class="content-lower">
+                <div class="followers">
+                    <p>${following} Followers <i class="fas fa-users"></i></p>
+                </div>
+                <div class="following">
+                    <p>${followers} Following <i class="fas fa-user"></i></p>
+                </div>
+            </div>`
+
             card.innerHTML = html
+
         })
+
 }
